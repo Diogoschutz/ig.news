941f15dea64c3cbfb783373656ba860e6b5ed003
Diogo Schutz <diogoschutz1@gmail.com>
2022-05-01 14:27:01 -0300

    estrutura conpleta da aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <h1>
+       Hello world! 
+    </h1>
+
+      <Head>
+        <title>início | ig.news</title>
+      </Head>
+    </>
+  )
+}
