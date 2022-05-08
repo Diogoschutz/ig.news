960cac564f0817e535a2cba221e70caadc5635ca
Diogo Schutz <diogoschutz1@gmail.com>
2022-05-08 20:19:19 -0300

    componente header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export function Header () {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a>Home</a>
+                    <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
             </div>
