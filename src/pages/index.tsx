34cf1e3cd3c7596544ca6004636e7181c11938c5
Diogo Schutz <diogoschutz1@gmail.com>
2022-05-14 09:10:33 -0300

    api stripe configuração

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
-import Head from 'next/head'
+import { GetServerSideProps} from 'next'
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -7,18 +11,26 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
 
-      <main>
-        <section>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/public/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+    }
+  }
 }
